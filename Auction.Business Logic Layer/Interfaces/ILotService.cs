13065ab9d52e56b137476c01d32f2793cace9533
New Name <profileenumerable@ukr.net>
2019-08-14 22:45:12 +0300

    [Auction.Wep Api] Get lots by page number in lot service was added

--- Auction.Business Logic Layer/Interfaces/ILotService.cs
+++ Auction.Business Logic Layer/Interfaces/ILotService.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Auction.Business_Logic_Layer.DTO;
 
 namespace Auction.Business_Logic_Layer.Interfaces
 {
     public interface ILotService
     {
-
+        IEnumerable<LotDto> GetLotsByPageNumber(int pageNum);
     }
 }
--- Auction.Business Logic Layer/Services/LotService.cs
+++ Auction.Business Logic Layer/Services/LotService.cs
@@ -1,8 +1,33 @@
-﻿using Auction.Business_Logic_Layer.Interfaces;
+﻿using Auction.Business_Logic_Layer.DTO;
+using Auction.Business_Logic_Layer.Interfaces;
+using Auction.Data_Access_Layer.Entities;
+using Auction.Data_Access_Layer.Interfaces;
+using Auction.Data_Access_Layer.Repositories;
+using AutoMapper;
+using System.Collections.Generic;
 
 namespace Auction.Business_Logic_Layer.Services
 {
     public class LotService : ILotService
     {
+        private IUnitOfWork _database { get; }
+
+        public LotService(IUnitOfWork database)
+        {
+            _database = database;
+        }
+
+        public IEnumerable<LotDto> GetLotsByPageNumber(int pageNum)
+        {
+            const int selectionLength = 10;//how many lots we can get for the requested page
+            int startId = selectionLength * (pageNum - 1);
+            int endId = startId + selectionLength;//set the and id of selection
+
+            IEnumerable<Lot> lots = _database.Lots.Find(l => l.Id > startId && l.Id <= endId);
+
+            IMapper mapper = new MapperConfiguration(cfg => cfg.CreateMap<Lot, LotDto>()).CreateMapper();
+
+            return mapper.Map<IEnumerable<Lot>, IEnumerable<LotDto>>(lots);
+        }
     }
 }
